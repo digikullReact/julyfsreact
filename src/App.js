8e9f84c3a6019244581699d9f60e2c35348b9c01
shubh <shubhamdixit863@gmail.com>
2022-10-21 19:16:52 +0530

    use state intro

--- src/App.js
+++ src/App.js
@@ -1,35 +1,98 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
-import { useState } from 'react';
 
-import Home from './components/Home';
-import Contact from './components/Contact';
+// what is state --->
+
+// state is just the data stored /showed /visible in your component at particular moment
+
+
+/**
+ * 
+ *    [{"price":7899,screensize:"23.8","company":"lenovo","discount":44,"refrate":"60hz":rating:4.4},{"price":7899,screensize:"23.8","company":"lenovo","discount":44,"refrate":"60hz":rating:4.4},
+ * {"price":7899,screensize:"23.8","company":"lenovo","discount":44,"refrate":"60hz":rating:4.4},{"price":7899,screensize:"23.8","company":"lenovo","discount":44,"refrate":"60hz":rating:4.4}]
+ */
+
+// price - 7899 
+// scrrensize-23.8 
+// company --Lenovo
+// discount- 44
+
+// refrate -60hz
+// rating -4.4
+
 
 
 function App() {
+  console.log(useState(0))
+ const [counter,setCounter] =useState(0)  // {}  // []
 
-  let  [state,setState]=useState(0);
-
-/// j svariabls or functions will come here -->
-
-const doSomething=()=>{
-
-//  value++;
-
-  //document.getElementById("heading").innerText=value;
-  setState(state++);
+ const [value,setValue] =useState("")  // {}  // []
 
 
-    
+
+const increment=()=>{
+
+  setCounter(counter+1)
+
+}
+
+const decrement=()=>{
+  setCounter(counter-1)
+
+
+
+}
+
+const change=(event)=>{
+
+  console.log(event.target.value);
+  setValue(event.target.value);
+
+
+
 }
 
 return(
-  <div>
-    <button onClick={doSomething}>
-  Click Me
-</button>
-<h1 id='heading'>{state}</h1>
+  <div style={{display:"flex" ,justifyContent:"space-evenly" ,marginTop:"70px"}}>
+    <div>
+    <button onClick={increment} className={"btn btn-success"}>
+ 
+ Increment
+    </button>
+
+    </div>
+
+    <div>
+    <button onClick={decrement} className={"btn btn-success"}>
+ 
+ Decrement
+    </button>
+
+    </div>
+
+    
+
+    <div>
+
+      <h1 id={"count"}>{counter}</h1>
+
+
+    </div>
+
+
+
+    <div>
+
+
+<input type={"text"}  onChange={change}/>
+
+
+    </div>
+
+
+
 
   </div>
 
